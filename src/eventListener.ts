a3c72b6a10e2b86351b1c15e0d7772ee07fe2b21
golem <sasteve999@gmail.com>
2023-10-09 11:48:20 +0300

    Fixed select

--- src/eventListener.ts
+++ src/eventListener.ts
@@ -7,21 +7,23 @@ import { convertImageDataToHexCode, getCanvasHTMLElement, getColorPickerModalHTM
 
 export const listenForClick = (mouseEvent: MouseEvent) => {
     const {
+        offsetX,
+        offsetY,
         x,
         y
     } = mouseEvent;
 
-    const imageData = getImageDataFromCoords(x,y);
+    const imageData = getImageDataFromCoords(offsetX,offsetY);
     const hexCode = convertImageDataToHexCode(imageData);
 
 
-    const imageDataForDisplay = getImageDataForDisplay(x,y);
+    const imageDataForDisplay = getImageDataForDisplay(offsetX,offsetY);
 
     const pixelMatrix = generatePixelMatrix(imageDataForDisplay);
 
     const colorPickerModalElement = getColorPickerModalHTMLElement();
 
-    const colorPickerModalHtml = ColorPickDisplayTemplate(pixelMatrix, x,y);
+    const colorPickerModalHtml = ColorPickDisplayTemplate(pixelMatrix, x, y);
 
     colorPickerModalElement.innerHTML = colorPickerModalHtml;
 
@@ -57,5 +59,5 @@ export const bindEventListener = () => {
     const canvasElement = getCanvasHTMLElement();
 
     canvasElement.addEventListener('mousedown', listenForClick);
-    // document.body.addEventListener('mouseup', listenForRelease);
+    document.body.addEventListener('mouseup', listenForRelease);
 }
